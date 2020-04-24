1cef44333cb96107860635768c3f7d61d1cdcb16
Anand Patil <anand@preceptlabs.com>
2020-04-24 22:12:36 +0530

    changes

--- src/Pages/Me/DashbBoard/MeSection/MeSection.jsx
+++ src/Pages/Me/DashbBoard/MeSection/MeSection.jsx
@@ -36,6 +36,7 @@ export class MeSection extends Component {
     if (!userId) {
       userId = this.props.match && this.props.match.params.id;
     }
+    console.log(userId, this.props.user, this.props.match.params.id);
     if (userId) {
       try {
         let api = API_BASE_URL + `users/${userId._id}`;
